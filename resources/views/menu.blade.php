1a5514ac6ca8ac5aa73927bf3f11ddbda4405a84
jupterLE <lucaspedroso1000@gmail.com>
2022-11-04 17:14:14 -0300

    test this app

---
+++ resources/views/menu.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu Principal</title>
+    <link rel="stylesheet" href="{{asset('style.css')}}">
+</head>
+<body>
+<div class="valortotal">
+   
+        <h2 class="txtreceber"> Valor a receber</h2>
+        <!-- fazer depois qndo tiver banco de dados->      <h1> " ?php $_POST['']   ?>" eae po</h1> -->
+</div>    
+
+
+<a link href="hoje.php">
+    <div class=receberhoje >
+        <h3 class=txthoje>A receber hoje</h3>
+         <!-- fazer depois qndo tiver banco de dados->      <h1> " ?php $_POST['']   ?>" eae po</h1> -->
+    </div>
+</a>
+
+<a link href="mes.php">
+<div class=recebermes>
+    <h3>A receber no mês</h3>
+     <!-- fazer depois qndo tiver banco de dados->      <h1> " ?php $_POST['']   ?>" eae po</h1> -->
+</div>
+</a>
+
+
+</body>
+</html>
--- resources/views/menu.php
+++
@@ -1,6 +0,0 @@
-<h1><?php echo $titulo;?></h1>
-
-<a href="mailto:sandroaf@unidavi.edu.br">
-    Enviar e-mail
-</a>
-
